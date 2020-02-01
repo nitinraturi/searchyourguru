dbcb216e26d9afce7410fa57300139ef4c518db0
nitin <raturi002@gmail.com>
2020-02-01 17:09:15 +0530

    Added diabled hpst check

--- frontend/searchyourguru/babel.config.js
+++ frontend/searchyourguru/babel.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  presets: ["@vue/cli-plugin-babel/preset"]
+  presets: ["@vue/cli-plugin-babel/preset"],
+  devServer: {
+    host: "0.0.0.0",
+    hot: true,
+    disableHostCheck: true
+  }
 };
