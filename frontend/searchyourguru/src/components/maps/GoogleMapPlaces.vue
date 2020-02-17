b8dee8f00c77588b71148bd930bc97fd4239af9f
rishu <rishabh.verma11998@gmail.com>
2020-02-17 16:35:15 +0530

    Add: Autocomplete google maps places component

---
+++ frontend/searchyourguru/src/components/maps/GoogleMapPlaces.vue
@@ -0,0 +1,27 @@
+<template>
+                <vue-google-autocomplete
+                    id="tution_area"
+                    classname="input"
+                    placeholder="eg: 110092 or Laxmi Nagar or Delhi"
+                    v-on:placechanged="getAddress"
+                    country="in"
+                >
+                </vue-google-autocomplete>
+</template>
+
+
+<script>
+import VueGoogleAutocomplete from 'vue-google-autocomplete'
+
+export default {
+    name: 'GoogleMapPlaces',
+    methods: {
+        getAddress: function(addressData, placeData, id) {
+            console.log(addressData);
+            console.log(placeData);
+            console.log(id);
+        }
+    },
+    components: { VueGoogleAutocomplete }
+}
+</script>
