c0e852893191f88dc86d3f2f13b024fc7a967801
rishu <rishabh.verma11998@gmail.com>
2020-02-19 19:45:26 +0530

    Add: Script to populate DB using pincodes from excel file

--- apps/users/utils.py
+++ apps/users/utils.py
@@ -1,5 +1,7 @@
 from rest_framework_simplejwt.tokens import RefreshToken
+from .models import AllZipCode
 import requests
+import pandas as pd
 
 
 def get_tokens_for_user(user):
@@ -17,3 +19,27 @@ def fetch_zipcode_from_api(zipcode):
     if response.status_code == 200:
         result = response.json()[0]
         return result['PostOffice'] if result['Status'] == 'Success' else None
+
+
+def fetch_zipcodes_from_excel_file(file_path):
+    df = pd.read_excel(file_path)
+    objects = []
+    try:
+        for pincode in df['PINCODE']:
+            pincode_data = fetch_zipcode_from_api(pincode)
+            if pincode_data is not None:
+                for data in pincode_data:
+                    obj = AllZipCode(
+                        po_name=data['Name'],
+                        district=data['District'],
+                        state=data['State'],
+                        country=data['Country'],
+                        zipcode=data['Pincode']
+                    )
+                    objects.append(obj)
+            else:
+                continue
+        AllZipCode.objects.bulk_create(objects)
+        print('Data has been inserted into the AllZipCode Table')
+    except:
+        print('Server error encountered')
