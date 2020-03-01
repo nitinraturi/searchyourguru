3f96494e604e870b91896aaf6805a710e0a4644d
Nitin Raturi <raturi002@gmail.com>
2020-03-01 10:36:25 +0530

    Fixed static files issue

--- conf/settings/base/emails.py
+++ conf/settings/base/emails.py
@@ -14,6 +14,6 @@ from decouple import config
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'helpdesk.syg@gmail.com '
+EMAIL_HOST_USER = 'helpdesk.syg@gmail.com'
 EMAIL_HOST_PASSWORD = "Syg@admin*"
 DEFAULT_FROM_EMAIL = 'Support <helpdesk@raturi.in>'
