a9b829d724ee5f4395738dd72fec2153c1157e20
Nitin Raturi <raturi002@gmail.com>
2020-03-01 11:34:35 +0530

    modified zipcode cron

--- apps/users/utils.py
+++ apps/users/utils.py
@@ -14,34 +14,35 @@ def get_tokens_for_user(user):
 
 
 def fetch_zipcode_from_api(zipcode):
-    zipcode_api_endpoint = "https://api.postalpincode.in/pincode/{zipcode}".format(zipcode=zipcode)
+    zipcode_api_endpoint = "https://api.postalpincode.in/pincode/{zipcode}".format(
+        zipcode=zipcode)
     response = requests.get(zipcode_api_endpoint)
-    print("response",response.status_code)
     if response.status_code == 200:
         result = response.json()[0]
-        print("result",result)
         return result['PostOffice'] if result['Status'] == 'Success' else None
 
 
 def fetch_zipcodes_from_excel_file(file_path):
     df = pd.read_excel(file_path)
-    objects = []
     try:
         for pincode in df['PINCODE']:
             pincode_data = fetch_zipcode_from_api(pincode)
             if pincode_data is not None:
                 for data in pincode_data:
-                    obj = AllZipCode(
-                        po_name=data['Name'],
-                        district=data['District'],
-                        state=data['State'],
-                        country=data['Country'],
-                        zipcode=data['Pincode']
-                    )
-                    objects.append(obj)
+                    try:
+                        AllZipCode.objects.create(
+                            po_name=data['Name'],
+                            district=data['District'],
+                            state=data['State'],
+                            country=data['Country'],
+                            zipcode=data['Pincode']
+                        )
+                        # objects.append(obj)
+                    except:
+                        pass
+
             else:
                 continue
-        AllZipCode.objects.bulk_create(objects)
         return True
     except:
         return None
