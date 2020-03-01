631740cb128f72f117d93791f8d5c2ed48cbfeb8
rishu <rishabh.verma11998@gmail.com>
2020-03-01 14:17:07 +0530

    Add: meta tags and page title for components

--- frontend/searchyourguru/src/router/index.js
+++ frontend/searchyourguru/src/router/index.js
@@ -16,6 +16,27 @@ const routes = [
     path: '/',
     name: 'home',
     component: Home,
+    meta: {
+      title: 'SearchYourGuru Home',
+      metaTags: [
+        {
+          name: 'description',
+          content: 'SearchYourGuru lets tutor to post tution details and student to search for a tution'
+        },
+        {
+          property: 'og:type',
+          content: 'website'
+        },
+        {
+          property: 'og:site_name',
+          content: 'SearchYourGuru'
+        },
+        {
+          property: 'og:url',
+          content: 'https://searchyourguru.com'
+        }
+      ]
+    },
     beforeEnter(to, from, next) {
       if (store.getters.is_user_tutor) next('/dashboard/')
       else next()
@@ -25,6 +46,27 @@ const routes = [
     path: '/search/',
     name: 'search',
     component: Search,
+    meta: {
+      title: 'SearchYourGuru Search Tution',
+      metaTags: [
+        {
+          name: 'description',
+          content: 'SearchYourGuru lets students to search tutions for a subject'
+        },
+        {
+          property: 'og_type',
+          content: 'website'
+        },
+        {
+          property: 'og:site_name',
+          content: 'SearchYourGuru'
+        },
+        {
+          property: 'og:url',
+          content: 'https://searchyourguru.com'
+        }
+      ]
+    },
     beforeEnter(to, from, next) {
       if (store.getters.is_user_tutor) next('/dashboard/')
       else next()
@@ -34,6 +76,27 @@ const routes = [
     path: '/login/',
     name: 'login',
     component: Login,
+    meta: {
+      title: 'SearchYourGuru Login',
+      metaTags: [
+        {
+          name: 'description',
+          content: 'SearchYourGuru Sign in or Log In as a student or tutor'
+        },
+        {
+          property: 'og_type',
+          content: 'website'
+        },
+        {
+          property: 'og:site_name',
+          content: 'SearchYourGuru'
+        },
+        {
+          property: 'og:url',
+          content: 'https://searchyourguru.com'
+        }
+      ]
+    },
     beforeEnter(to, from, next) {
       if (store.getters.is_loggedin) next('/dashboard/')
       else next()
@@ -43,6 +106,27 @@ const routes = [
     path: '/signup/',
     name: 'signup',
     component: Signup,
+    meta: {
+      title: 'SearchYourGuru Sign Up | Registration',
+      metaTags: [
+        {
+          name: 'description',
+          content: 'SearchYourGuru Sign Up or Register as a student or tutor'
+        },
+        {
+          property: 'og_type',
+          content: 'website'
+        },
+        {
+          property: 'og:site_name',
+          content: 'SearchYourGuru'
+        },
+        {
+          property: 'og:url',
+          content: 'https://searchyourguru.com'
+        }
+      ]
+    },
     beforeEnter(to, from, next) {
       if (store.getters.is_loggedin) next('/dashboard/')
       else next()
@@ -52,6 +136,27 @@ const routes = [
     path: '/verification/',
     name: 'verification',
     component: Verification,
+    meta: {
+      title: 'SearchYourGuru Verification',
+      metaTags: [
+        {
+          name: 'description',
+          content: 'SearchYourGuru email verification for users'
+        },
+        {
+          property: 'og_type',
+          content: 'website'
+        },
+        {
+          property: 'og:site_name',
+          content: 'SearchYourGuru'
+        },
+        {
+          property: 'og:url',
+          content: 'https://searchyourguru.com'
+        }
+      ]
+    },
     beforeEnter(to, from, next) {
       if (store.getters.is_loggedin) next('/dashboard/')
       else next()
@@ -61,6 +166,27 @@ const routes = [
     path: '/password-reset/',
     name: 'password_reset',
     component: PasswordReset,
+    meta: {
+      title: 'SearchYourGuru password reset',
+      metaTags: [
+        {
+          name: 'description',
+          content: 'SearchYourGuru user password reset or forgot password'
+        },
+        {
+          property: 'og_type',
+          content: 'website'
+        },
+        {
+          property: 'og:site_name',
+          content: 'SearchYourGuru'
+        },
+        {
+          property: 'og:url',
+          content: 'https://searchyourguru.com'
+        }
+      ]
+    },
     beforeEnter(to, from, next) {
       if (store.getters.is_loggedin) next('/dashboard/')
       else next()
@@ -70,6 +196,23 @@ const routes = [
     path: '/dashboard/',
     name: 'dashboard',
     component: Dashboard,
+    meta: {
+      title: 'SearchYourGuru Dashboard',
+      metaTags: [
+        {
+          name: 'description',
+          content: 'SearchYourGuru lets you create tution and modify tution details'
+        },
+        {
+          property: 'og:type',
+          content: 'website'
+        },
+        {
+          property: 'og:site_name',
+          content: 'SearchYourGuru'
+        }
+      ]
+    },
     beforeEnter(to, from, next) {
       if (!store.getters.is_loggedin) next('/login/')
       else next()
@@ -77,9 +220,35 @@ const routes = [
   }
 ]
 
+
 const router = new VueRouter({
   routes,
   mode: 'history'
+})
+
+router.beforeEach((to, from, next) => {
+  const nearestWithTitle = to.matched.slice().reverse().find(r => r.meta && r.meta.title)
+  const nearestWithMeta = to.matched.slice().reverse().find(r => r.meta && r.meta.metaTags)
+
+  if(nearestWithTitle) document.title = nearestWithTitle.meta.title
+
+  Array.from(document.querySelectorAll('[data-vue-router-controlled]')).map(el => el.parentNode.removeChild(el))
+
+  if(!nearestWithMeta) return next()
+
+  nearestWithMeta.meta.metaTags.map(tagDef => {
+    const tag = document.createElement('meta')
+
+    Object.keys(tagDef).forEach(key => {
+      tag.setAttribute(key, tagDef[key])
+    })
+
+    tag.setAttribute('data-vue-router-controlled', '')
+
+    return tag
+  })
+  .forEach(tag => document.head.appendChild(tag))
+  next()
 })
 
 export default router
