218a8485828e7a134c7bf755233a747a7a9613a1
Nitin Raturi <raturi002@gmail.com>
2020-03-04 10:46:25 +0530

    Indentation fixed

--- frontend/searchyourguru/src/components/forms/LocationFieldForm.vue
+++ frontend/searchyourguru/src/components/forms/LocationFieldForm.vue
@@ -46,7 +46,7 @@ export default {
       let keyCode = e.which || e.keyCode
       if (
         (keyCode >= 65 && keyCode <= 90) ||
-        (keyCode >= 97 && keyCode <= 122)||
+        (keyCode >= 97 && keyCode <= 122) ||
         (keyCode >= 43 && keyCode <= 53) ||
         (keyCode >= 29 && keyCode <= 54) ||
         (keyCode >= 7 && keyCode <= 16)
