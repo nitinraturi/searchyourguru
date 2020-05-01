e09c422b851c6f55405669d0929950d0335cdf4c
= <raturi002@gmail.com>
2020-05-01 18:01:20 +0530

    Fixed email entry

--- conf/settings/base/emails.py
+++ conf/settings/base/emails.py
@@ -1,7 +1,4 @@
 from decouple import config
-# SERVER_EMAIL = ''
-# DEFAULT_FROM_EMAIL = ''
-# ADMINS = (('Your name', 'your@email.here'),)
 
 # Email Configuration
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
@@ -10,10 +7,3 @@ EMAIL_PORT = config('EMAIL_PORT', cast=int)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
-
-# EMAIL_USE_TLS = True
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_HOST_USER = 'helpdesk.syg@gmail.com'
-# EMAIL_HOST_PASSWORD = "Syg@admin*"
-# DEFAULT_FROM_EMAIL = 'Support <helpdesk@raturi.in>'
